250537841a491b39321ed2e9661d025e81ddea7f
Hugo Guitton <guitton.h@gmail.com>
2020-06-14 17:00:49 +0200

    Twitter clone - StimulusReflex

---
+++ db/migrate/20200614121528_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :username, null: false, default: "TestUser"
+      t.string :body, null: false
+      t.integer :likes_count, null: false, default: 0
+      t.integer :reposts_count, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
